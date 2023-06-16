a43f26142ef81e086704ae4f7bb1be6239617070
Fabrizio <fabrinucci99@gmail.com>
2023-06-16 14:13:40 -0300

    Update navigation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,45 @@
+import { MouseEvent } from 'react';
 import style from './Header.module.css';
 
 export const Header = () => {
+  const handleNavigation = (
+    event: MouseEvent<HTMLAnchorElement>,
+    sectionName: string
+  ) => {
+    event.preventDefault();
+    const targetElement = document.querySelector(`#${sectionName}`);
+    targetElement?.scrollIntoView({
+      behavior: 'smooth',
+    });
+  };
+
   return (
     <header id="home" className={style.Header}>
       <nav className={style.Nav}>
-        <a href="#home">Home</a>
-        <a href="#about">About Us</a>
-        <a href="#portfolio">Portfolio</a>
-        <a href="#services">Services</a>
-        <a href="#contact">Contact</a>
+        <a onClick={(event) => handleNavigation(event, 'home')} href="#home">
+          Home
+        </a>
+        <a onClick={(event) => handleNavigation(event, 'about')} href="#about">
+          About Us
+        </a>
+        <a
+          onClick={(event) => handleNavigation(event, 'portfolio')}
+          href="#portfolio"
+        >
+          Portfolio
+        </a>
+        <a
+          onClick={(event) => handleNavigation(event, 'services')}
+          href="#services"
+        >
+          Services
+        </a>
+        <a
+          onClick={(event) => handleNavigation(event, 'contact')}
+          href="#contact"
+        >
+          Contact
+        </a>
       </nav>
       <section className={style['Header-text']}>
         <h1>Boost your sales by 200%</h1>
